da370edab2eceaadc8b4ab7de0436ba394497265
Cestnut <hunterkudo@gmail.com>
2023-08-13 18:37:42 +0200

    minor change

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@ root_path = "/home/giov/database_NoSQL-MongoDB_Neo4j"
 
 def write_csv(file_path, csv_list):
     with open(file_path+".csv", "w") as file:
-            print("Scrivendo il file {}\n\n".format(file_path))    
+            print("Scrivendo il file {}".format(file_path))    
             for record in csv_list:
                 record = [str(value) for value in record]
                 newLine = ",".join(record)+"\n"
