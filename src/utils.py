d6227c4508bfb907e10a68b50fc8faef8c7b457b
Cestnut <hunterkudo@gmail.com>
2023-08-18 17:41:28 +0200

    changed copy_dataset. Instead of doing the work of copying part of the original files, just copies the file of specified size (25,50,75 or 100)

--- src/utils.py
+++ src/utils.py
@@ -17,30 +17,20 @@ def write_csv(file_path, csv_list):
             print("Scritto il file {}\n\n".format(file_path))    
             file.close()
 
-#Ritorna il numero di record nel file csv (escludendo la prima linea con gli head)
-def csv_len(file_path):
-    p = subprocess.run(["wc", "-l", file_path], stdout=subprocess.PIPE, text=True)
-    output = int(p.stdout.strip().split()[0]) - 1
-    return output
-
 def copy_dataset(percentage, debug):
 
     if not os.path.exists(input_csv):
             os.makedirs(input_csv)
     
     if debug: print("Copiando il dataset")
-    for file_name in os.listdir(original_csv):
-        with open(original_csv+file_name, 'r') as input_file,  open(input_csv+file_name, 'w') as output_file:
+    for file_name in ["people", "calls", "cells"]:
+        input_file_name = file_name + str(percentage) + ".csv"
+        output_file_name = file_name + ".csv"
+        with open(original_csv+input_file_name, 'r') as input_file,  open(input_csv+output_file_name, 'w') as output_file:
 
-            reader = csv.reader(input_file)
-            writer = csv.writer(output_file)
-
-            input_len = csv_len(original_csv+file_name)
-            output_len = int((input_len*(percentage/100)) + 1)
-
-            for _ in range(output_len):
-                row = next(reader)
-                writer.writerow(row)
+            for line in input_file:
+                output_file.write(line)
+                
     if debug: print("Finito di copiare il dataset")
 
 def write_results(dbms, size_percentage, times):
