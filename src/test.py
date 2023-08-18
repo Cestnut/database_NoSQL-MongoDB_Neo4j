763396b5f08dc7bdc4f56be3f0edb3ead84f75dd
Cestnut <hunterkudo@gmail.com>
2023-08-18 17:18:25 +0200

    removed nocache_time and added onlycache_time option

--- src/test.py
+++ src/test.py
@@ -17,38 +17,39 @@ def reset_containers():
 def insert_data(database_handler, buffer_size=0, debug=True):
     database_handler.inserter.insert_all_data(debug=debug)
 
-def read(database_handler, begin, end, city, iterations, nocache_time=False, debug=True):
+def read(database_handler, begin, end, city, iterations, onlycache_time=False, debug=True):
     no_cache_times = list()
     normal_times = list()
-    if nocache_time:
-        no_cache_times.append(["QUERY_1", "QUERY_2", "QUERY_3","QUERY_4","QUERY_5"])
-        no_cache_tmp = list()
-        i = 1
-        for query in database_handler.reader.read_queries:
-            database_handler.clear_cache()
-            execution_time = database_handler.reader.read_query(query)
-            print("Query eseguite (no cache): {}".format(i))
-            i+=1
-            no_cache_tmp.append(execution_time)
-        no_cache_times.append(no_cache_tmp)
-        
-    #Il seguente serve per popolare la cache delle query
+    
+    no_cache_times.append(["QUERY_1", "QUERY_2", "QUERY_3","QUERY_4","QUERY_5"])
+    no_cache_tmp = list()
     i = 1
     for query in database_handler.reader.read_queries:
-        database_handler.reader.read_query(query)
-        print("Query eseguite (popolando la cache): {}".format(i))
+        database_handler.clear_cache()
+        execution_time = database_handler.reader.read_query(query)
+        print("Query eseguite (no cache): {}".format(i))
         i+=1
+        no_cache_tmp.append(execution_time)
+    no_cache_times.append(no_cache_tmp)
 
-    normal_times.append(["QUERY_1", "QUERY_2", "QUERY_3","QUERY_4","QUERY_5"])
-    j = 1
-    for i in range(iterations):
-        normal_iteration = list()
+    if onlycache_time is False: 
+    #Il seguente serve per popolare la cache delle query
+        i = 1
         for query in database_handler.reader.read_queries:
-            execution_time = database_handler.reader.read_query(query)
-            print("Query eseguite: {}".format(j))
-            j+=1
-            normal_iteration.append(execution_time)
-        normal_times.append(normal_iteration)
+            database_handler.reader.read_query(query)
+            print("Query eseguite (popolando la cache): {}".format(i))
+            i+=1
+    
+        normal_times.append(["QUERY_1", "QUERY_2", "QUERY_3","QUERY_4","QUERY_5"])
+        j = 1
+        for i in range(iterations):
+            normal_iteration = list()
+            for query in database_handler.reader.read_queries:
+                execution_time = database_handler.reader.read_query(query)
+                print("Query eseguite: {}".format(j))
+                j+=1
+                normal_iteration.append(execution_time)
+            normal_times.append(normal_iteration)
 
     return no_cache_times, normal_times
 
@@ -71,7 +72,7 @@ if __name__ == "__main__":
     parser.add_argument('--mongo_insert_buffer_size', 
                         help="Dimensione del buffer su cui caricare i record prima che vengano inseriti nel database(default: 10000)")
     parser.add_argument('--reset', help="True/False, se resettare i container all'avvio del test (default:False)")
-    parser.add_argument('--nocache_time', help="True/False, se fare per la prima volta le query senza usare la cache (default:True)")
+    parser.add_argument('--onlycache_time', help="True/False, se fare per la prima volta le query senza usare la cache (default:True)")
 
     args = parser.parse_args()
 
@@ -84,7 +85,7 @@ if __name__ == "__main__":
     neo = ast.literal_eval(args.neo4j) if args.neo4j else True
     mongo_insert_buffer_size = int(args.mongo_insert_buffer_size) if args.mongo_insert_buffer_size else 10000
     reset = ast.literal_eval(args.reset) if args.reset else False
-    nocache_time = ast.literal_eval(args.nocache_time) if args.nocache_time else True
+    onlycache_time = ast.literal_eval(args.onlycache_time) if args.onlycache_time else True
 
     data_set_percentage = [25,50,75,100]
     for percentage in data_set_percentage:
@@ -94,7 +95,7 @@ if __name__ == "__main__":
         if mongo:
             mongo_handler = MongoHandler(begin_timestamp, end_timestamp, city, utils.root_path, insert_buffer_size=mongo_insert_buffer_size)
             insert_data(mongo_handler, debug=debug)
-            result = read(mongo_handler, begin_timestamp, end_timestamp, city, iterations, nocache_time=nocache_time, debug=debug)
+            result = read(mongo_handler, begin_timestamp, end_timestamp, city, iterations, onlycache_time=onlycache_time, debug=debug)
             utils.write_results("mongo", percentage, result)
             utils.rearrange_results("mongo")
 
@@ -102,6 +103,6 @@ if __name__ == "__main__":
         if neo:
             neo_handler = NeoHandler(begin_timestamp, end_timestamp, city, timeout=120)
             insert_data(neo_handler, debug=debug)
-            result = read(neo_handler, begin_timestamp, end_timestamp, city, iterations, nocache_time=nocache_time, debug=debug)
+            result = read(neo_handler, begin_timestamp, end_timestamp, city, iterations, onlycache_time=onlycache_time, debug=debug)
             utils.write_results("neo4j", percentage, result)
             utils.rearrange_results("neo4j")
